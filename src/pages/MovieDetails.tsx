964742d9031ca6a3fef0b30b8c5683f93dc2e647
Victor <aue228lublupacanov@gmail.com>
2025-06-04 20:00:39 +0200

    forgot to add .env to gitignore

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,7 +1,45 @@
-export default function MovieDetails() {
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { getMovieDetails } from "../api/tmdb"
+
+interface MovieDetails {
+    id: number
+    title: string
+    overview: string
+    poster_path: string | null
+    release_date: string
+}
+
+const MovieDetails = () => {
+    const { id } = useParams()
+    const [movie, setMovie] = useState<MovieDetails | null>(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            try {
+                if (id) {
+                    const data = await getMovieDetails(id)
+                    setMovie(data)
+                }
+            } catch (err) {
+                setError((err as Error).message)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchMovie()
+    }, [id])
+    if (loading) return <p>Laoding...</p>
+    if (error) return <p style={{ color: 'red' }}>{error}</p>
+    if (!movie) return <p>Film was not found:</p>
+
     return (
-        <>
-            <h2>MovieDetails</h2>
-        </>
+        <div>
+            <h2>{ }</h2>
+        </div>
     )
+
 }
